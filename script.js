68bf2abc34401789fd2f541ee3b4894ac54f1603
Jon Keegan <jon@jonkeegan.com>
2013-11-26 00:53:22 -0500

    bootstrap, using d3.csv to load data

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 var time_seq = [];
 var heading = [];
 
+var m = [10, 10, 10, 10]; // margins
+var width = 960 - m[1] - m[3]; // width
+var height = 500 - m[0] - m[2]; // height
+
 // set the width and height of the chart
-var width = 960, height = 500;
+//var width = 960, height = 500;
 
 // set the domain and range for each axis
 var x = d3.scale.linear().domain([0, 500]).range([0, width]);
@@ -15,57 +19,54 @@ var line = d3.svg.line()
     .x(function(d,i) { return x(d.x); })
     .y(function(d) { return y(d.y); });
 
-
+var raw_log = d3.text("nostrand.csv");
+var sensor_data = [];
+var svg;
+var motionAttributes;
+var this_chart_data_1 = [];
 function init(){
-console.log("init!");
-
-var svg = d3.select("#chart").append("svg:svg")
+	console.log("init!");
+	
+	// load the raw data...
+	d3.csv("nostrand.csv", function(data) {
+       //on success...
+       sensor_data=data
+       
+       // loop through the object
+       $.each(data, function(index, val){
+		  // console.log(" asdas"+index);
+		 
+		  this_chart_data_1.push({x:index, y:val.motionRotationRateZ});
+		   
+		});
+	
+		init_chart();
+	}); // end csv load success
+} // end init
+	  
+	  
+function init_chart(){
+	 svg = d3.select("#chart").append("svg:svg")
     .attr("width", width)
     .attr("height", height)
     .attr("class", "line")
-	.append("svg:g");
-					
-var this_chart_data_1 = [];
-var this_chart_data_2 = [];
-var this_chart_data_3 = [];
-var this_chart_data_4 = [];
-var this_chart_data_5 = [];
-
-
-
-var this_val = [];
-$.each(sensor_data, function(index, val){
-	//console.log(val.TrueHeading)
-
-	 this_motionAttributes = val;
+        .append("svg:g");
+        
+        
+	// setup left nav
+	
+	var this_motionAttributes = sensor_data[0];
+	
+	$.each(this_motionAttributes, function(index, val){
+                motionAttributes.push(index);
+                $("#controls").append("<a href='#' class='list-group-item lgi-compact'>"+index+"</a>");
+    })
+    
+    x = d3.scale.linear().domain([0, sensor_data.length]).range([0, width]),
+	y = d3.scale.linear().domain([-50, 50]).range([height, 0]);
 	
 	
-	
-	time_seq.push(index);
-	heading.push(val.accelerationZ);
-	this_chart_data_1.push({x:index, y:val.motionRotationRateZ});
-	this_chart_data_2.push({x:index, y:val.accelerationZ});
-	this_chart_data_3.push({x:index, y:val.accelerationX});
-	this_chart_data_4.push({x:index, y:val.accelerationY});
-	this_chart_data_5.push({x:index, y:val.RotationZ});
-	
-	 
- });
- 
- $.each(this_motionAttributes, function(index_2, val_2){
-		motionAttributes.push(index_2);
-		$("#controls").append("<div>"+index_2+"</div>");
-	})
-
-/*
-x = d3.scale.linear().domain([d3.min(time_seq), d3.max(time_seq)]).range([0, width]),
-y = d3.scale.linear().domain([d3.min(heading), d3.max(heading)]).range([200, 0]);
-*/
-
-x = d3.scale.linear().domain([0, time_seq.length]).range([0, width]),
-y = d3.scale.linear().domain([-50, 50]).range([height, 0]);
-
- svg.append("svg:path")
+	svg.append("svg:path")
     .data([this_chart_data_1])
     .attr("class", "line")
        .attr("fill", "none")
@@ -74,42 +75,8 @@ y = d3.scale.linear().domain([-50, 50]).range([height, 0]);
        .attr("d", line)
        .enter().append("svg:path");
 
-            
- svg.append("svg:path")
-    .data([this_chart_data_2])
-    .attr("class", "line")
-       .attr("fill", "none")
-       .attr("stroke", "orange")
-       .attr("stroke-width", 2)
-       .attr("d", line);
-	   
-  svg.append("svg:path")
-    .data([this_chart_data_3])
-    .attr("class", "line")
-       .attr("fill", "none")
-       .attr("stroke", "green")
-       .attr("stroke-width", 2)
-       .attr("d", line);
-  
-  
-	  
-  svg.append("svg:path")
-    .data([this_chart_data_4])
-    .attr("class", "line")
-       .attr("fill", "none")
-       .attr("stroke", "purple")
-       .attr("stroke-width", 2)
-       .attr("d", line);
-   
-  svg.append("svg:path")
-    .data([this_chart_data_5])
-    .attr("class", "line")
-       .attr("fill", "none")
-       .attr("stroke", "blue")
-       .attr("stroke-width", 2)
-       .attr("d", line);
-  
-     	
+    
+             
     svg.selectAll(".xLabel")
     .data(x.ticks(5))
     .enter().append("svg:text")
@@ -129,11 +96,9 @@ y = d3.scale.linear().domain([-50, 50]).range([height, 0]);
     .attr("dy", ".35em")
     .attr("text-anchor", "end")
     .text(y.tickFormat(5));
-  
-     	
-     	
-     }
-	  
+	
+	
+}	  
 
 
 
